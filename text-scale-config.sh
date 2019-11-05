bc27067ffcba9924f5fd896c0c7147985f09c9c1
Dmitry Sorokin <molotov.dmitry@gmail.com>
2019-11-05 15:26:37 +0300

    Added support of all connected displays.

--- text-scale-config.sh
+++ text-scale-config.sh
@@ -94,53 +94,69 @@ disableautostart()
     echo "autostart=false" > "${HOME}/.config/text-scale-config/setup-done"
 }
 
-displayinfo="$(xrandr --current | grep ' connected' | head -n1)"
-Xaxis=$(echo "${displayinfo}" | awk '{print $4}' | cut -d '+' -f 1 | cut -d 'x' -f 1)
-Yaxis=$(echo "${displayinfo}" | awk '{print $4}' | cut -d '+' -f 1 | cut -d 'x' -f 2)
+getscale()
+{
+    local defaultdpi=96
+    local sizepx="$1"
+    local sizemm="$2"
+    
+    if [[ $sizepx -le 0 ]] || [[ $sizemm -le 0 ]]
+    then
+        return 1
+    fi
+    
+    local dpi="$(echo "${sizepx} / (${sizemm} / 25.4)" | bc -l)"
+    local scalelong="$(echo "${dpi} / 96" | bc -l)"
+    local scale="$(LC_NUMERIC=C printf "%.2f" "${scalelong}" | sed '/\./ s/\.\{0,1\}0\{1,\}$//')"
+    
+    if [[ -z "$scale" || "$scale" == '0' ]]
+    then
+        return 1
+    fi
+    
+    echo "${scale}"
+}
 
-Xsize=$(echo "${displayinfo}" | grep -o '[[:digit:]]*mm' | head -n1 | sed 's/mm$//')
-Ysize=$(echo "${displayinfo}" | grep -o '[[:digit:]]*mm' | tail -n1 | sed 's/mm$//')
+roundscale()
+{
+    LC_NUMERIC=C printf "%.1f" "$1" | sed '/\./ s/\.\{0,1\}0\{1,\}$//'
+}
 
-Xdpi="$(echo "${Xaxis} / (${Xsize} / 25.4)" | bc -l)"
-Ydpi="$(echo "${Yaxis} / (${Ysize} / 25.4)" | bc -l)"
-
-Xscalelong=$(echo "${Xdpi} / 96" | bc -l)
-Yscalelong=$(echo "${Ydpi} / 96" | bc -l)
-
-Xscale=$(LC_NUMERIC=C printf "%.2f" "${Xscalelong}")
-Yscale=$(LC_NUMERIC=C printf "%.2f" "${Yscalelong}")
-
-Xscaleshort=$(LC_NUMERIC=C printf "%.1f" "${Xscalelong}")
-Yscaleshort=$(LC_NUMERIC=C printf "%.1f" "${Yscalelong}")
+#### Globals ===================================================================
 
 unset options
 declare -a options
 
-options=('1.0')
+options=('1.0' '1.25' '1.5' '2.0')
 
-if [[ -n "$Xscale" ]]
-then
-    options+=("${Xscale}")
-fi
+#### Get displays DPI list =====================================================
 
-if [[ -n "$Yscale" && "${Yscale}" != "${Xscale}" ]]
-then
-    options+=("${YScale}")
-fi
+while read -r displayinfo
+do
+    sizepx="$(echo "${displayinfo}" | grep -o '[[:digit:]]\+x[[:digit:]]\+')"
+    sizespx=("${sizepx%%x*}" "${sizepx##*x}")
 
-if [[ -n "$Xscaleshort" && "${Xscaleshort}0" != "${Xscale}" ]]
-then
-    options+=("${Xscaleshort}")
-fi
+    sizemm=$(echo "${displayinfo}" | grep -o '[[:digit:]]\+mm' | sed 's/mm$//' | tr '\n' 'x' | sed 's/x$//')
+    sizesmm=("${sizemm%%x*}" "${sizemm##*x}")
 
-if [[ -n "$Yscaleshort" && "${Yscaleshort}" != "${Xscaleshort}" && "${Yscaleshort}0" != "${Yscale}" ]]
-then
-    options+=("${Yscaleshort}")
-fi
+    for i in 0 1
+    do
+        scale="$(getscale "${sizespx[$i]}" "${sizesmm[$i]}")"
+        
+        options+=("$scale")
+        options+=("$(roundscale "$scale")")
+    done
+done < <(LC_ALL=C xrandr | grep ' connected')
+
+#### Sort and remove duplicates from scales list ===============================
+
+readarray -t scales < <(for a in "${options[@]}"; do echo "$a"; done | sort -g | uniq)
+
+#### Select and apply scale ====================================================
 
 while true
 do
-    newscale="$(selectvalue 'Text scaling factor' 'Please select text scaling factor:' "${options[@]}")"
+    newscale="$(selectvalue 'Text scaling factor' 'Please select text scaling factor:' "${scales[@]}")"
     
     if [[ -n "${newscale}" ]]
     then
@@ -166,5 +182,7 @@ do
     break
 
 done
+
+#### Disable autostart =========================================================
 
 disableautostart
